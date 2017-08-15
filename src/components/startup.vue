ddd7921866c22031aba2435638c08e0c7486d8e7
chenglei <871379171@qq.com>
2017-08-15 13:50:03 +0800

    上传文件

---
+++ src/components/startup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="start">
+       <p class="army-font">犯我强汉者<br>虽远必诛！</p>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default{}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.start{
+  text-align: center;
+  position: fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+}
+</style>
